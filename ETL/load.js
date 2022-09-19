b9eebb17bfa85d9783868860e32e86bbb76acf71
Nick Adam <na4hx@virginia.edu>
2022-09-19 16:57:49 -0400

    uncomments the load ETL

--- ETL/load.js
+++ ETL/load.js
@@ -14,22 +14,22 @@ var loadCSV = (csvFilePath, table) => {
 
 
 // products
-// loadCSV('/users/nickadam/documents/hackreactor/Products/raw_data/product.csv', 'products(id, name, slogan, description, category, default_price)')
+ loadCSV('/users/nickadam/documents/hackreactor/Products/raw_data/product.csv', 'products(id, name, slogan, description, category, default_price)')
 
 // features
-// loadCSV(`/users/nickadam/documents/hackreactor/Products/raw_data/features.csv`, 'features(id, product_id, feature, value)')
+ loadCSV(`/users/nickadam/documents/hackreactor/Products/raw_data/features.csv`, 'features(id, product_id, feature, value)')
 
 // styles
-// loadCSV(`/users/nickadam/documents/hackreactor/Products/raw_data/styles.csv`, 'styles(style_id, product_id, name, sale_price, original_price, "default?")')
+ loadCSV(`/users/nickadam/documents/hackreactor/Products/raw_data/styles.csv`, 'styles(style_id, product_id, name, sale_price, original_price, "default?")')
 
 // skus
-// loadCSV(`/users/nickadam/documents/hackreactor/Products/raw_data/skus.csv`, 'skus(id, styleId, size, quantity)')
+ loadCSV(`/users/nickadam/documents/hackreactor/Products/raw_data/skus.csv`, 'skus(id, styleId, size, quantity)')
 
 // photos
-// loadCSV(`/users/nickadam/documents/hackreactor/Products/raw_data/photos.csv`, 'photos(id, styleId, url, thumbnail_url)')
+ loadCSV(`/users/nickadam/documents/hackreactor/Products/raw_data/photos.csv`, 'photos(id, styleId, url, thumbnail_url)')
 
 // related
-// loadCSV(`/users/nickadam/documents/hackreactor/Products/raw_data/related.csv`, 'related(id, current_product_id, current_related_id)')
+ loadCSV(`/users/nickadam/documents/hackreactor/Products/raw_data/related.csv`, 'related(id, current_product_id, current_related_id)')
 
 
 
