bc1ec6c49bf46d1169907318d9605786db6abf01
Nick Adam <na4hx@virginia.edu>
2022-09-20 12:16:39 -0400

    creates connection to remote database

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg');
+require('dotenv').config();
+
 
 const pool = new Pool({
+  host: 'ec2-18-222-198-230.us-east-2.compute.amazonaws.com',
   database: 'products',
   user: 'nickadam',
-  password: process.env.PGPASSWORD
+  password: process.env.PGPASSWORD,
+  port: '5432'
 });
+
 
 pool.connect(err => {
     if (err) {
